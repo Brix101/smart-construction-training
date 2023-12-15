85405474cc5480daf2fbbb6ebdfa9d375b0958db
Brix <brixter@1000miles.biz>
2023-12-15 14:35:51 +0800

    feat: added topics data view

--- src/lib/actions/topic.ts
+++ src/lib/actions/topic.ts
@@ -2,11 +2,56 @@
 
 import { db } from "@/db"
 import { topics } from "@/db/schema"
-import { and, eq } from "drizzle-orm"
-import { revalidatePath } from "next/cache"
+import { and, eq, not } from "drizzle-orm"
+import { unstable_noStore as noStore, revalidatePath } from "next/cache"
 import { z } from "zod"
 
-import { getTopicSchema } from "@/lib/validations/topic"
+import { getTopicSchema, topicSchema } from "@/lib/validations/topic"
+
+const extendedTopicSchema = topicSchema.extend({
+  courseId: z.number(),
+})
+
+export async function checkTopic(input: { name: string; id?: number }) {
+  noStore()
+  try {
+    const topicWithSameName = await db.query.topics.findFirst({
+      columns: {
+        id: true,
+      },
+      where: input.id
+        ? and(not(eq(topics.id, input.id)), eq(topics.name, input.name))
+        : eq(topics.name, input.name),
+    })
+
+    if (topicWithSameName) {
+      throw new Error("Topic name already taken.")
+    }
+  } catch (err) {
+    console.error(err)
+    return null
+  }
+}
+
+export async function addTopic(input: z.infer<typeof extendedTopicSchema>) {
+  const topicWithSameName = await db.query.topics.findFirst({
+    columns: {
+      id: true,
+    },
+    where: eq(topics.name, input.name),
+  })
+
+  if (topicWithSameName) {
+    throw new Error("Topic name already taken.")
+  }
+
+  await db.insert(topics).values({
+    ...input,
+    courseId: input.courseId,
+  })
+
+  revalidatePath(`/dashboard/courses/${input.courseId}/topics.`)
+}
 
 export async function deleteTopic(rawInput: z.infer<typeof getTopicSchema>) {
   const input = getTopicSchema.parse(rawInput)
--- src/lib/validations/topic.ts
+++ src/lib/validations/topic.ts
@@ -1,5 +1,12 @@
 import * as z from "zod"
 
+export const topicSchema = z.object({
+  name: z.string().min(3).max(256),
+  urlId: z.string().max(100),
+  videoLink: z.string().min(3).max(256),
+  details: z.string().optional(),
+})
+
 export const getTopicSchema = z.object({
   id: z.number(),
   courseId: z.number(),
