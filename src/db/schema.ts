8044e24304f618aae19034369690ddbfb149bc8f
Brix <brixter@1000miles.biz>
2023-12-15 11:35:27 +0800

    feat: added course column query

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,3 +1,4 @@
+import { relations } from "drizzle-orm"
 import {
   integer,
   pgTable,
@@ -7,31 +8,35 @@ import {
   varchar,
 } from "drizzle-orm/pg-core"
 
-export const courses = pgTable(
-  "courses",
-  {
-    id: serial("id").primaryKey(),
-    name: varchar("name", { length: 256 }),
-    urlId: varchar("url_id", { length: 100 }),
-    youtubeUrl: text("youtube_irl"),
-    createdAt: timestamp("created_at"),
-    updatedAt: timestamp("updated_at"),
-  },
-  //   courses => {
-  //     return {
-  //       nameIndex: uniqueIndex("name_idx").on(courses.name),
-  //     }
-  //   },
-)
+export const courses = pgTable("courses", {
+  id: serial("id").primaryKey(),
+  name: varchar("name", { length: 256 }),
+  urlId: varchar("url_id", { length: 100 }),
+  youtubeUrl: text("youtube_irl"),
+  createdAt: timestamp("created_at"),
+  updatedAt: timestamp("updated_at"),
+})
 
 export type Course = typeof courses.$inferSelect
 export type NewCourse = typeof courses.$inferInsert
 
+export const coursesRelations = relations(courses, ({ many }) => ({
+  materials: many(materials),
+}))
+
 export const materials = pgTable("materials", {
   id: serial("id").primaryKey(),
   name: varchar("name", { length: 256 }).default(""),
+  link: text("link"),
   courseId: integer("course_id").references(() => courses.id),
 })
 
 export type Material = typeof materials.$inferSelect
 export type NewMaterial = typeof materials.$inferInsert
+
+export const materialsRelations = relations(materials, ({ one }) => ({
+  course: one(courses, {
+    fields: [materials.courseId],
+    references: [courses.id],
+  }),
+}))
