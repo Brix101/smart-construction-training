7507e9624cadeb19c24cfc694e1d7b40f0e2da9b
Brixter Porras <brixterporras.com>
2024-02-25 22:34:49 +0800

    feat: added topic loader

--- src/app/(lobby)/courses/[courseId]/loading.tsx
+++ src/app/(lobby)/courses/[courseId]/loading.tsx
@@ -1,3 +1,40 @@
+import { Icons } from "@/components/icons"
+import { TopicCardSkeleton } from "@/components/skeletons/topic-card-skeleton"
+import { Skeleton } from "@/components/ui/skeleton"
+
 export default async function LoadingPage() {
-  return <div>Loading ...</div>
+  return (
+    <>
+      <div className="flex flex-col">
+        <div className="bg-blue-100">
+          <div className="container flex w-full flex-col justify-between space-y-5 pb-2 pt-4 md:flex-row md:space-y-0 md:pt-1">
+            <div className="flex items-center space-x-1 text-sm text-muted-foreground">
+              <Skeleton className="h-4 w-20 bg-foreground" />
+              <Icons.chevronRight size={15} />
+              <Skeleton className="h-4 w-64 bg-foreground" />
+            </div>
+            <Skeleton className="h-12 w-64 border" />
+          </div>
+        </div>
+        <div className="relative">
+          <div className="absolute inset-0 bg-gradient-to-t from-transparent to-blue-100" />
+          <div className="container relative flex space-x-4 pt-2">
+            <div className="space-y-1 pb-2">
+              <Skeleton className="h-10 w-64 bg-foreground" />
+            </div>
+          </div>
+          <div className="w-full">
+            <div className="container flex translate-y-1 space-x-4">
+              <div className="h-12"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="container grid grid-cols-1 gap-6 pt-4 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {Array.from({ length: 8 }).map((_, i) => (
+          <TopicCardSkeleton key={i} />
+        ))}
+      </div>
+    </>
+  )
 }
---
+++ src/components/skeletons/topic-card-skeleton.tsx
@@ -0,0 +1,18 @@
+import { AspectRatio } from "@/components/ui/aspect-ratio"
+import { Card, CardHeader } from "@/components/ui/card"
+import { Skeleton } from "@/components/ui/skeleton"
+
+export function TopicCardSkeleton() {
+  return (
+    <Card className="h-full overflow-hidden rounded-sm">
+      <AspectRatio ratio={4 / 3}>
+        <div className="absolute inset-0 bg-gradient-to-t from-transparent to-zinc-950/50" />
+        <Skeleton className="absolute right-2 top-2 h-6 w-14 rounded-sm px-2 py-1" />
+        <Skeleton className="h-full w-full rounded-b-none" />
+      </AspectRatio>
+      <CardHeader className="space-y-2">
+        <Skeleton className="h-4 w-1/2" />
+      </CardHeader>
+    </Card>
+  )
+}
