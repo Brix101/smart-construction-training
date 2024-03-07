a98ee4df3e83c51a688f938a62d4db9159c3d028
Brixter Porras <brixterporras@gmail.com>
2024-03-07 10:42:10 +0800

    feat: added tailwind indicator

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,11 @@
 import { ThemeProvider } from "@/components/providers"
+import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { Toaster } from "@/components/ui/toaster"
 import { siteConfig } from "@/config/site"
 import "@/styles/globals.css"
 import { ClerkProvider } from "@clerk/nextjs"
 import { Analytics } from "@vercel/analytics/react"
-import type { Metadata } from "next"
+import type { Metadata, Viewport } from "next"
 import { Inter } from "next/font/google"
 
 const inter = Inter({ subsets: ["latin"] })
@@ -14,11 +15,15 @@ export const metadata: Metadata = {
   description: siteConfig.description,
 }
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export const viewport: Viewport = {
+  colorScheme: "dark light",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+}
+
+export default function RootLayout({ children }: React.PropsWithChildren) {
   return (
     <>
       <ClerkProvider>
@@ -31,6 +36,7 @@ export default function RootLayout({
               disableTransitionOnChange
             >
               {children}
+              <TailwindIndicator />
             </ThemeProvider>
             <Toaster />
             <Analytics />
