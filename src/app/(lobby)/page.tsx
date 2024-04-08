b4fdc5af5b206b2c01b2f7c69ed4529c9f265359
Brixter Porras <brixterporras@gmail.com>
2024-04-08 17:50:19 +0800

    refactor: style courses page

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -17,7 +17,7 @@ export default async function IndexPage() {
         <PageHeaderHeading>Courses</PageHeaderHeading>
         <PageHeaderDescription>Explore Courses</PageHeaderDescription>
       </PageHeader>
-      <section className="grid grid-cols-1 gap-6 pt-14 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+      <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {allCourses.map(course => (
           <CourseCard
             key={course.id}
