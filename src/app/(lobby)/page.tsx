77e577d6943c9e11916bbd508e81527b751203cc
Brixter Porras <brixterporras@gmail.com>
2024-04-08 17:51:47 +0800

    refactor: style courses page

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -15,7 +15,7 @@ export default async function IndexPage() {
     <Shell>
       <PageHeader className="container">
         <PageHeaderHeading>Courses</PageHeaderHeading>
-        <PageHeaderDescription>Explore Courses</PageHeaderDescription>
+        <PageHeaderDescription>Explore available courses</PageHeaderDescription>
       </PageHeader>
       <section className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {allCourses.map(course => (
