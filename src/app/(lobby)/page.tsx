aedf2bca2da9f9022e215da07b5e086a7c5f2e39
Brixter Porras <brixterporras@gmail.com>
2024-04-08 17:39:03 +0800

    refactor: style courses page

--- src/app/(lobby)/page.tsx
+++ src/app/(lobby)/page.tsx
@@ -7,29 +7,17 @@ import {
 import { Shell } from "@/components/shells/shell"
 import { getPublishedCourses } from "@/lib/actions/course"
 
-export default async function HomePage() {
+export default async function IndexPage() {
   const coursePromises = await getPublishedCourses()
   const [allCourses] = await Promise.all([coursePromises])
 
   return (
-    <>
-      <div
-        className="w-full bg-background py-5"
-        // style={{
-        //   backgroundImage: "url(/svg/default_banner.svg)",
-        //   backgroundSize: "cover",
-        //   backgroundPosition: "80% 20%",
-        // }}
-      >
-        <PageHeader className="container">
-          <PageHeaderHeading>Courses</PageHeaderHeading>
-          <PageHeaderDescription>Explore Courses</PageHeaderDescription>
-        </PageHeader>
-      </div>
-      <Shell
-        variant="default"
-        className="grid grid-cols-1 gap-6 pt-14 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4"
-      >
+    <Shell>
+      <PageHeader className="container">
+        <PageHeaderHeading>Courses</PageHeaderHeading>
+        <PageHeaderDescription>Explore Courses</PageHeaderDescription>
+      </PageHeader>
+      <section className="grid grid-cols-1 gap-6 pt-14 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {allCourses.map(course => (
           <CourseCard
             key={course.id}
@@ -37,7 +25,7 @@ export default async function HomePage() {
             href={`/courses/${course.id}`}
           />
         ))}
-      </Shell>
-    </>
+      </section>
+    </Shell>
   )
 }
