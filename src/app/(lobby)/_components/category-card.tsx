4dc220649a190471dbc8571d204f5a5932826aaa
Brixter Porras <brixterporras@gmail.com>
2025-03-03 22:35:33 +0800

    style: category card

--- src/app/(lobby)/_components/category-card.tsx
+++ src/app/(lobby)/_components/category-card.tsx
@@ -9,6 +9,7 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card"
+import { getRandomPatternStyle } from "@/lib/generate-pattern"
 
 interface CategoryCardProps {
   category: Awaited<ReturnType<typeof getCategoryList>>[0]
@@ -16,22 +17,27 @@ interface CategoryCardProps {
 }
 
 export function CategoryCard({ category, href }: CategoryCardProps) {
-  const imageSrc = category.imgSrc ?? "/placeholder.svg"
-
   return (
     <Link href={href}>
       <span className="sr-only">{category.name}</span>
       <Card className="h-full overflow-hidden transition-colors hover:bg-muted/50">
         <AspectRatio ratio={21 / 9}>
           <div className="absolute inset-0 bg-gradient-to-t from-transparent to-primary/70" />
-          <Image
-            src={imageSrc}
-            alt={category.name}
-            className="object-cover"
-            sizes="(min-width: 1024px) 20vw, (min-width: 768px) 25vw, (min-width: 640px) 33vw, (min-width: 475px) 50vw, 100vw"
-            fill
-            loading="lazy"
-          />
+          {category.imgSrc ? (
+            <Image
+              src={category.imgSrc}
+              alt={category.name}
+              className="object-cover"
+              sizes="(min-width: 1024px) 20vw, (min-width: 768px) 25vw, (min-width: 640px) 33vw, (min-width: 475px) 50vw, 100vw"
+              fill
+              loading="lazy"
+            />
+          ) : (
+            <div
+              className="h-full rounded-t-md border-b"
+              style={getRandomPatternStyle(category.id)}
+            />
+          )}
         </AspectRatio>
         <CardHeader className="space-y-2">
           <CardTitle className="line-clamp-1">{category.name}</CardTitle>
