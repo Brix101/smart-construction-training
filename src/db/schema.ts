030bed174aef771133dc8f9e67275d55963b53d5
Brixter Porras <brixterporras@gmail.com>
2025-03-03 18:48:12 +0800

    feat: added category schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -7,6 +7,7 @@ export const courses = pgTable("courses", (t) => ({
   description: t.text(),
   level: t.integer().notNull().default(1),
   sequence: t.integer().notNull().default(0),
+  imgSrc: t.text(),
   isPublished: t.boolean().notNull().default(false),
   isActive: t.boolean().notNull().default(true),
   createdAt: t.timestamp().defaultNow().notNull(),
@@ -20,6 +21,7 @@ export type NewCourse = typeof courses.$inferInsert
 
 export const coursesRelations = relations(courses, ({ many }) => ({
   topics: many(topics),
+  categories: many(categories),
 }))
 
 export const topics = pgTable("topics", (t) => ({
@@ -97,6 +99,53 @@ export const topicsToMaterialsRelations = relations(
     topic: one(topics, {
       fields: [topicMaterials.topicId],
       references: [topics.id],
+    }),
+  })
+)
+
+export const categories = pgTable("categories", (t) => ({
+  id: t.uuid().notNull().primaryKey().defaultRandom(),
+  name: t.varchar({ length: 256 }).unique().notNull(),
+  description: t.text(),
+  imgSrc: t.text(),
+  isActive: t.boolean().notNull().default(true),
+  createdAt: t.timestamp().defaultNow().notNull(),
+  updatedAt: t
+    .timestamp({ mode: "string", withTimezone: true })
+    .$onUpdateFn(() => sql`now()`),
+}))
+
+export type Category = typeof categories.$inferSelect
+
+export const categoryRelations = relations(categories, ({ many }) => ({
+  courses: many(courses),
+}))
+
+export const courseCategories = pgTable(
+  "course_categories",
+  (t) => ({
+    courseId: t
+      .uuid()
+      .notNull()
+      .references(() => courses.id, { onDelete: "cascade" }),
+    categoryId: t
+      .uuid()
+      .notNull()
+      .references(() => categories.id, { onDelete: "cascade" }),
+  }),
+  (t) => [primaryKey({ columns: [t.courseId, t.categoryId] })]
+)
+
+export const courseCategoriesRelations = relations(
+  courseCategories,
+  ({ one }) => ({
+    course: one(courses, {
+      fields: [courseCategories.courseId],
+      references: [courses.id],
+    }),
+    category: one(categories, {
+      fields: [courseCategories.categoryId],
+      references: [categories.id],
     }),
   })
 )
