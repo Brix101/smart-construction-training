399d46221f275d2d3887838bf1037cb25c441c32
Brixter Porras <brixterporras@gmail.com>
2025-03-10 10:52:53 +0800

    fix: category caching issue

--- src/app/_actions/category.ts
+++ src/app/_actions/category.ts
@@ -43,36 +43,27 @@ export async function getCategories() {
 }
 
 export async function getCategoryList() {
-  return await unstable_cache(
-    async () => {
-      try {
-        return await db
-          .select({
-            id: categories.id,
-            name: categories.name,
-            imgSrc: categories.imgSrc,
-            description: categories.description,
-            courseCount: countDistinct(courseCategories.courseId),
-          })
-          .from(categories)
-          .leftJoin(
-            courseCategories,
-            eq(categories.id, courseCategories.categoryId)
-          )
-          .groupBy(categories.id, courseCategories.categoryId)
-          .where(eq(categories.isActive, true))
-          .orderBy(asc(categories.name))
-      } catch (error) {
-        console.error(error)
-        return []
-      }
-    },
-    ["category-list"],
-    {
-      revalidate: 3600, // every hour
-      tags: ["category-list"],
-    }
-  )()
+  try {
+    return await db
+      .select({
+        id: categories.id,
+        name: categories.name,
+        imgSrc: categories.imgSrc,
+        description: categories.description,
+        courseCount: countDistinct(courseCategories.courseId),
+      })
+      .from(categories)
+      .leftJoin(
+        courseCategories,
+        eq(categories.id, courseCategories.categoryId)
+      )
+      .groupBy(categories.id, courseCategories.categoryId)
+      .where(eq(categories.isActive, true))
+      .orderBy(asc(categories.name))
+  } catch (error) {
+    console.error(error)
+    return []
+  }
 }
 
 export async function getCategory(id: Category["id"]) {
